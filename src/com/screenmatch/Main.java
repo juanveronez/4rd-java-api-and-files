ae8b8f7da715465861f0009500d7cf5a07444c6b
Juan Veronez <juan.monteirov@gmail.com>
2025-11-17 11:48:34 -0300

    use an user input to search a movie into omdbapi

--- src/com/screenmatch/Main.java
+++ src/com/screenmatch/Main.java
@@ -5,13 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Busque pelo nome do filme: ");
+        String search = scanner.nextLine();
+
+        String uriString = "https://www.omdbapi.com/?t=%s&apikey=594fa5ec".formatted(search);
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?apikey=594fa5ec&t=matrix"))
+                .uri(URI.create(uriString))
                 .build();
 
         HttpResponse<String> response = client
